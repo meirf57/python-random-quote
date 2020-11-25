643129a6bcd3d2b40b4230b2ec0c50fafd85b2f2
Meir Fainzilber <fainzilberm@gmail.com>
2020-11-25 16:04:46 -0300

    finished and implemented plus

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,19 @@
 import random
 
 def primary():
-  
+  f = open("quotes.txt", "a")
+  f.writelines("See you soon!\n")
+  f.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  rnd = random.randint(0,len(quotes)-1)
+  print(quotes[random.randint(0,len(quotes)-1)][:-1]
+    +"\n"+
+    quotes[random.randint(0,len(quotes)-1)][:-1])
 
-  print(quotes[rnd])
+  print(f"file len: {len(quotes)}")
 
 if __name__== "__main__":
   primary()
